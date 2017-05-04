461deb88ee465392a963e7de5e16407a62dbe36a
PlainviewData <daniel@plainview.io>
2017-05-04 12:51:01 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var sentiment = require('../node_modules/sentimentz');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/sentiment', function(req, res, next){
+  res.json({result: (sentiment(req.body.phrase).score >= 0 ? "Positive" : "Negative")})
+});
+
+router.post('/sentiment_batch', function(req, res, next){
+  console.log(req.body);
+  console.log(JSON.parse(req.body))
+  req.body = JSON.parse(req.body);
+  var results = [];
+  req.body.phrases.forEach(function(phrase){
+    results.push((sentiment(req.body).score >= 0 ? "Positive" : "Negative"));
+  });
+  res.json(results);
+});
+
+module.exports = router;
